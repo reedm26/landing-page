e5de9e1cf189aa98f5df5ab225463bb4fe04834f
reedm26 <reedm26@yahoo.com>
2020-04-23 04:18:29 -0600

    adding

--- src/views/About.vue
+++ src/views/About.vue
@@ -15,51 +15,38 @@
             </a>
             <h6 class="card-category text-info">Web Designer</h6>
             <div class="card-footer">
-              <a href="#pablo" class="btn btn-icon btn-neutral btn-lg btn-twitter">
+              <a
+                href="#pablo"
+                class="btn btn-icon btn-neutral btn-lg btn-twitter"
+              >
                 <i class="fab fa-twitter"></i>
               </a>
-              <a href="#pablo" class="btn btn-icon btn-neutral btn-lg btn-dribbble">
+              <a
+                href="#pablo"
+                class="btn btn-icon btn-neutral btn-lg btn-dribbble"
+              >
                 <i class="fab fa-dribbble"></i>
               </a>
-              <a href="#pablo" class="btn btn-icon btn-neutral btn-lg btn-instagram">
+              <a
+                href="#pablo"
+                class="btn btn-icon btn-neutral btn-lg btn-instagram"
+              >
                 <i class="fab fa-instagram"></i>
               </a>
             </div>
           </card>
         </div>
-        <div class="col-md-6 col-lg-3">
-          <card type="profile">
-            <img slot="avatar" class="img img-raised" src="img/mike.jpg" />
-            <div class="card-body">
-              <h4 class="card-title">Alec Mike</h4>
-              <h6 class="category text-gray">Designer</h6>
-              <p class="card-description">
-                One of the co-founders. Alec drives the technical strategy of the platform, customer
-                support and brand.
-              </p>
-              <div class="card-footer">
-                <a href="#pablo" class="btn btn-icon btn-twitter btn-round">
-                  <i class="fab fa-twitter"></i>
-                </a>
-                <a href="#pablo" class="btn btn-icon btn-facebook btn-round">
-                  <i class="fab fa-facebook-square"></i>
-                </a>
-                <a href="#pablo" class="btn btn-icon btn-google btn-round">
-                  <i class="fab fa-google"></i>
-                </a>
-              </div>
-            </div>
-          </card>
-        </div>
+
         <div class="col-md-6 col-lg-3">
           <card type="profile" color="black">
             <img slot="avatar" class="img img-raised" src="img/james.jpg" />
             <div>
               <h6 class="card-category">CEO / Co-Founder</h6>
               <h4 class="card-title">James Thompson</h4>
-              <p
-                class="card-description"
-              >Don't be scared of the truth because we need to restart the human foundation in truth.</p>
+              <p class="card-description">
+                Don't be scared of the truth because we need to restart the
+                human foundation in truth.
+              </p>
               <a href="#pablo" class="btn btn-primary btn-round">Follow</a>
             </div>
           </card>
@@ -76,13 +63,22 @@
               <h4 class="card-title">Olivia Thompson</h4>
               <p class="card-description">"Work hard play harder."</p>
               <div class="card-footer">
-                <a href="#pablo" class="btn btn-icon btn-primary btn-simple btn-round">
+                <a
+                  href="#pablo"
+                  class="btn btn-icon btn-primary btn-simple btn-round"
+                >
                   <i class="fab fa-twitter"></i>
                 </a>
-                <a href="#pablo" class="btn btn-icon btn-primary btn-simple btn-round">
+                <a
+                  href="#pablo"
+                  class="btn btn-icon btn-primary btn-simple btn-round"
+                >
                   <i class="fab fa-facebook-square"></i>
                 </a>
-                <a href="#pablo" class="btn btn-icon btn-primary btn-simple btn-round">
+                <a
+                  href="#pablo"
+                  class="btn btn-icon btn-primary btn-simple btn-round"
+                >
                   <i class="fab fa-google"></i>
                 </a>
               </div>
@@ -93,6 +89,4 @@
     </div>
   </div>
 </template>
-<script>
-</script>
-
+<script></script>
