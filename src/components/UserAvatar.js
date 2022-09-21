106f80f85e4decdcbf2672f217fca7f293dcea55
Marcel Reed <reedm26@yahoo.com>
2022-09-21 00:49:47 -0600

    refactor name for user avatar

---
+++ src/components/UserAvatar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Avatar} from "@mui/material";
+
+const UserAvatar = () => {
+    return (
+<div>
+    <Avatar>MR</Avatar>
+</div>
+    );
+};
+
+export default UserAvatar;
